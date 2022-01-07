559786268a2e8176aa655f607bdf861d36f9c90a
yogeshhere1999@gmail.com <yogeshhere1999@gmail.com>
2022-01-07 12:07:54 +0530

    printZ

---
+++ Basics of programming/getting started/print_z.java
@@ -0,0 +1,14 @@
+public class print_z {
+
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+
+    }
+}
+    
+
