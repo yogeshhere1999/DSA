dfa11813895e9a166691c5eb5a191dedb6e491d8
yogeshhere1999@gmail.com <yogeshhere1999@gmail.com>
2022-03-07 23:06:36 +0530

    2D array Demo

---
+++ Basics of programming/2D array/2DArrayDemo.java
@@ -0,0 +1,25 @@
+import java.util.*;
+  
+  public class 2DArrayDemo{
+  
+  public static void main(String[] args) {
+    // write your code here 
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int m = scn.nextInt();
+    int[][] arr = new int[n][m];
+
+    for(int i = 0; i<n;i++){
+        for(int j = 0; j<m ;j++){
+          arr[i][j] = scn.nextInt();
+        }
+    } 
+
+       for(int i = 0; i<n;i++){
+        for(int j = 0; j<m ;j++){
+          System.out.print(arr[i][j]+" ");
+        }
+        System.out.println();
+    }
+   }
+  }
