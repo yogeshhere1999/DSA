bc86669f7494773511bb008b38370ccbb24f001c
yogeshhere1999@gmail.com <yogeshhere1999@gmail.com>
2022-03-10 18:20:56 +0530

    palindromic substring

--- Basics of programming/String,StringBuilderAndArrayList.java/PrintAllPalindromicSubstring.java
+++ Basics of programming/String,StringBuilderAndArrayList.java/PrintAllPalindromicSubstring.java
@@ -1,5 +1,43 @@
-package Basics of programming.String,StringBuilderAndArrayList.java;
+
+   
+   
 
 public class PrintAllPalindromicSubstring {
-    
+	public static void solution(String str){
+		//write your code here
+		for(int i =0;i<str.length();i++){
+			for(int j = i+1;j<=str.length();j++){
+				String ss = str.substring(i,j);
+				if(isPalin(ss)==true){
+					System.out.println(ss);
+				}
+			}
+		
+		}
+		
+	}
+
+	public static boolean isPalin(String ss){
+		int i = 0;
+		int j = ss.length()-1;
+
+		while(i<=j){
+			char ch1 = ss.charAt(i);
+			char ch2 = ss.charAt(j);
+			if(ch1!=ch2){
+				return false;
+			}else{
+				i++;
+				j--;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		solution(str);
+	}
+
 }
