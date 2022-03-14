447fde3a357633b1e11a0d437f898db7ce4d0852
yogeshhere1999@gmail.com <yogeshhere1999@gmail.com>
2022-03-14 10:22:20 +0530

    print increasing

---
+++ recursion/Introduction to recursion/printIncreasing.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class printincreasing {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner (System.in);
+        int n = scn.nextInt();
+        printIncreasing(n);
+    }
+
+    public static void printIncreasing(int n){
+        if(n == 0){
+            return ;
+        }
+        printIncreasing(n-1);
+        System.out.println(n);
+    }
+
+}
